8877870589d2e74ee10d29c6eccd8ec491e78696
Franck D'agostini <franck.dagostini@gmail.com>
2024-04-10 22:16:51 +0200

    add test to recipes json request specs

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -4,6 +4,7 @@ describe 'Recipes', type: :request do
   before do
     Recipe.create!(name: 'Salade de tomates', ingredients: '2 tomates', locale: 'fr')
     Recipe.create!(name: 'Salade de carottes', ingredients: '2 carottes', locale: 'fr')
+    Recipe.create!(name: 'Salade de carottes aux épices', ingredients: '2 carottes, des épices', locale: 'fr')
   end
 
   context 'when json requests' do
@@ -21,10 +22,37 @@ describe 'Recipes', type: :request do
       end
 
       it 'displays recipes' do
-        json = JSON.parse(response.body)
+        recipes = JSON.parse(response.body)
 
-        expect(json.size).to eq(1)
-        expect(json[0]['name']).to eq('Salade de carottes')
+        expect(recipes.size).to eq(2)
+
+        recipe_names = recipes.map { |recipe| recipe['name'] }
+        expect(recipe_names).to include('Salade de carottes')
+        expect(recipe_names).to include('Salade de carottes aux épices')
+      end
+    end
+
+    describe 'GET / with ingredients' do
+      before do
+        get '/fr', headers: { 'ACCEPT' => 'application/json' },
+                   params: { search_form: { ingredients: %w[carot epice] } }
+      end
+
+      it 'responds with a 200 status' do
+        expect(response).to be_successful
+      end
+
+      it 'is a json response' do
+        expect(response.content_type).to include('application/json')
+      end
+
+      it 'displays recipes' do
+        recipes = JSON.parse(response.body)
+        expect(recipes.size).to eq(1)
+
+        recipe_names = recipes.map { |recipe| recipe['name'] }
+        expect(recipe_names).not_to include('Salade de carottes')
+        expect(recipe_names).to include('Salade de carottes aux épices')
       end
     end
   end
